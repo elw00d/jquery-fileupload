17273e6b0114a215337d34bf0a836367c10b5b52
igor.kostromin <igor.kostromin@team.dz.ru>
2012-08-16 16:12:34 +0400

    * Fixed RU localization to work with another scripts modifying default settings.

--- java/src/main/webapp/js/jquery.fileupload-ru.js
+++ java/src/main/webapp/js/jquery.fileupload-ru.js
@@ -1,11 +1,13 @@
 /* Russian (UTF-8) initialisation for the jQuery file upload plugin. */
 jQuery(function($) {
-    $.fileupload.regional['ru'] = {
+    if ($.fileupload.regional['ru'] == undefined || $.fileupload.regional['ru'] == null)
+        $.fileupload.regional['ru'] = { };
+    $.extend($.fileupload.regional['ru'], {
         clientAbortMessage: 'Загрузка прервана пользователем',
         sizeLimitExceededMessage: 'Превышен допустимый размер файла',
         internalErrorMessage: 'При загрузке произошла ошибка, попробуйте ещё раз',
         filesNotSelectedMessage: 'Выберите файл',
         uploadNotFinishedYetMessage: 'Дождитесь завершения загрузки или отмените её нажатием ESC'
-    };
+    });
     $.fileupload.setDefaults($.fileupload.regional['ru']);
 });
